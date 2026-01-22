7ecb835d5a2504d83bdab3ab8a3a18a70487ecd3
JV13 <cherkas.ivan13@gmail.com>
2026-01-22 21:07:39 +0300

    работа с путями

--- weather.js
+++ weather.js
@@ -1,16 +1,25 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
 import { printHelp } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
 
 const initCli = () => {
   const args = getArgs(process.argv)
   if(args.h){
-    printHelp('sadfsdf')
+
+    printHelp('HELP')
+
   } else if (args.s) {
+
     console.log('save City')
-  } else if (args.t) {
-    console.log('token')
+
+  } else if (args.t) 
+
+    {
+      saveKeyValue( 'token', args.t )
+
   } else {
+    
     console.log('weather')
   }
 }
